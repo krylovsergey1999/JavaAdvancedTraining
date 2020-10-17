02856b09985ac378ed8df6e76f2b146b7f38c048
Krylov Sergey <kssergeyru@gmail.com>
2020-10-17 22:02:20 +0300

    L-08-JMM

---
+++ L-08-JMM/src/main/java/ru/advancedtraining/jmm/CounterSynchronized.java
@@ -0,0 +1,55 @@
+package ru.advancedtraining.jmm;
+
+/**
+ * @author Krylov Sergey (sergey.krylov@lanit-tercom.com) (17.10.2020)
+ */
+public class CounterSynchronized {
+	private int count = 0;
+	private static final int LIMIT = 100_000_000;
+	private final Object monitor = new Object();
+
+	public static void main(String[] args) throws InterruptedException {
+		CounterSynchronized counter = new CounterSynchronized();
+		counter.go();
+	}
+
+	/*
+		private synchronized void inc() {
+			for (int i = 0; i < LIMIT; i++) {
+				count++;
+			}
+		}
+	*/
+/*
+    private void inc() {
+        synchronized (this) {
+            for (int i = 0; i < LIMIT; i++) {
+                count++;
+            }
+        }
+    }
+*/
+	private void inc() {
+		// тут никакой многопоточности нет, потоки по очереди берут монитор и делают работу
+		synchronized (monitor) {
+			for (int i = 0; i < LIMIT; i++) {
+				count++;
+			}
+		}
+	}
+
+	private void go() throws InterruptedException {
+		Thread thread1 = new Thread(this::inc);
+		Thread thread2 = new Thread(this::inc);
+		Thread thread3 = new Thread(this::inc);
+
+		thread1.start();
+		thread2.start();
+		thread3.start();
+
+		thread1.join();
+		thread2.join();
+		thread3.join();
+		System.out.println("CounterBroken:" + count);
+	}
+}
